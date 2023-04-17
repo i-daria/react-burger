3d14d98051eedd52cec1f011505c7fde2db505be
Darya <ilinykhdarya@yandex.ru>
2023-04-17 14:43:53 +0500

    Updates

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const getIngredients = (store) => store.ingredients.ingredients;
-export const getSelectedIngredients = (store) => store.ingredients.selectedIngredients;
-export const getModalIsOpen = (store) => store.modal.isOpen;
-export const getCurrentIngredient = (store) => store.ingredients.currentIngredient;
-export const getOrder = (store) => store.order.order;
