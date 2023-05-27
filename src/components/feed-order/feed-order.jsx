e123e9fd08fa40cb6dbf67b2da5a986aebc8f9f6
Darya <ilinykhdarya@yandex.ru>
2023-05-27 14:27:39 +0500

    верстка и отображение остальных ингредиентов в заказе в Ленте заказов по клику

--- src/components/feed-order/feed-order.jsx
+++ src/components/feed-order/feed-order.jsx
@@ -7,6 +7,7 @@ import PropTypes from "prop-types";
 import { useSelector } from 'react-redux';
 
   export const FeedOrder = React.memo(({order}) => {
+
   const navigate = useNavigate();
   const location = useLocation();
   const ingredientsData = useSelector(store => store.ingredients.ingredients) 
@@ -14,25 +15,37 @@ import { useSelector } from 'react-redux';
     const item = ingredientsData.find(el => el._id === ingredient);
     return item;
   });
+
   let ingredientsCount = 6;
   let moreCount = null;
+  let moreCountList = [];
+  const [showMore, setShowMore] = React.useState(false);
   if (ingredients.length <= 6 ) {
     ingredientsCount = ingredients.length;
   } else {
     moreCount = ingredients.length - 6; 
+    moreCountList = ingredients.slice(6);
   };
+
   const total = ingredients.reduce((acc, item) => acc + item.price , 0);
 
   const isVisible = location.pathname === '/profile/orders';
-  
-  
-  const onClick = () => {
-    if (location.pathname === '/feed') {
-      navigate(`/feed/${order._id}`, {state: { background: location}});
-    };
-    if (location.pathname === '/profile/orders') { 
-      navigate(`/profile/orders/${order._id}`, {state: { background: location}});
-    }
+    
+  const onClick = (e) => {
+    if (!e.target.classList.contains(styles.show_more_text)) {
+      if (location.pathname === '/feed') {
+        navigate(`/feed/${order._id}`, {state: { background: location}});
+      };
+      if (location.pathname === '/profile/orders') { 
+        navigate(`/profile/orders/${order._id}`, {state: { background: location}});
+      }
+    } 
+  };
+
+  const showMoreIngredients = (e) => {
+    e.target.style.display = 'none';
+    e.target.closest(`.${styles.ingredient_last_background}`).classList.remove(styles.ingredient_last_background);
+    setShowMore(true);
   };
 
   return order && (
@@ -55,9 +68,17 @@ import { useSelector } from 'react-redux';
         <ul className={styles.list}>
           {ingredients.slice(0, ingredientsCount).map((ingredient, index) => {
             return (
-              <li className={styles.ingredient} key={index} >
+              <li className={`${styles.ingredient} ${moreCount && index === 0 ? styles.ingredient_last_background : ''}`} key={index}>
                 <img className={styles.img} src={ingredient.image} alt={ingredient.name}  />
-                { moreCount && (<span className={styles.ingredient_last}> +{moreCount} </span>)}   
+                { (moreCount && index === 0) && (<span className={styles.show_more_text} onClick={showMoreIngredients} > +{moreCount} </span>)}   
+              </li>
+            ); 
+            })
+          }  
+          {showMore &&  moreCountList.map((ingredient, index) => {
+            return (
+              <li className={styles.ingredient} key={index + 6} >
+                <img className={styles.img} src={ingredient.image} alt={ingredient.name}  /> 
               </li>
             ); 
             })
